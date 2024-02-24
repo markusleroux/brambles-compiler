daca1b15854bf49f99f8142ae539eda63b7eadb7
Markus Le Roux <markusleroux314@gmail.com>
2024-02-24 18:39:40 -0500

    Initial lexing and parsing

---
+++ src/AST.hs
@@ -0,0 +1,48 @@
+module AST where
+
+
+data Op
+    = Add
+    | Sub
+    | Mult
+    | Div
+    deriving (Eq, Ord, Show)
+
+data Type
+    = TInt
+    | TFloat
+    deriving (Eq, Ord, Show)
+
+type Name = String
+
+data Variable
+    = UntypedVar Name
+    | TypedVar Type Name
+    deriving (Eq, Ord, Show)
+
+data Expr
+    = IntLit Integer
+    | FloatLit Double
+    | Variable Variable
+    | BinOp Op Expr Expr
+    | Call Name [Expr]
+    | Assignment Expr Expr
+    deriving (Eq, Ord, Show)
+
+data Block = Block [Expr]
+    deriving (Eq, Ord, Show)
+
+data Function = Function
+    { functionName :: Name
+    , functionArguments :: [Variable]
+    , functionReturnType :: Type
+    , functionBody :: Block
+    }
+    deriving (Eq, Ord, Show)
+
+data Program = Program
+    { globals :: [Expr]
+    , functions :: [Function]
+    }
+    deriving (Eq, Ord, Show)
+
---
+++ src/Lexer.hs
@@ -0,0 +1,44 @@
+module Lexer where
+
+import Text.Parsec
+import qualified Text.Parsec.Token as Tok
+
+lexer :: Tok.TokenParser m
+lexer = Tok.makeTokenParser languageDef
+    where
+        opParser = oneOf ":!#$%&*+./<=>?@\\^|-~"
+        languageDef = Tok.LanguageDef
+            { Tok.commentStart = "/*"
+            , Tok.commentEnd = "*/"
+            , Tok.commentLine = "//"
+            , Tok.nestedComments = False
+            -- Identifiers
+            , Tok.identStart = letter <|> char '_'
+            , Tok.identLetter = alphaNum <|> char '_'
+            -- Ops
+            , Tok.opStart = opParser
+            , Tok.opLetter = opParser
+            -- Reserved
+            , Tok.reservedNames = ["fn", "int", "float"]
+            , Tok.reservedOpNames = ["+", "-", "*", "/", ";", "->", "="]
+            -- Other
+            , Tok.caseSensitive = True
+            }
+
+spaceConsumer = Tok.whiteSpace lexer
+
+integerType = Tok.reserved lexer "int"
+floatType = Tok.reserved lexer "float"
+identifier = Tok.identifier lexer
+
+integer = Tok.integer lexer
+float = Tok.float lexer
+
+parens = Tok.parens lexer
+braces = Tok.braces lexer 
+commas = Tok.commaSep lexer
+semicolon = Tok.reservedOp lexer ";"
+
+fn = Tok.reserved lexer "fn"
+returnArrow = Tok.reservedOp lexer "->"
+assignment = Tok.reservedOp lexer "="
