19a963056ccf7ace8e903bd7843b9b64078d4f65
Markus Le Roux <markusleroux314@gmail.com>
2024-02-25 15:05:57 -0500

    Basic REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
+import Parser
+
+import Text.Parsec (parse)
+import System.Console.Haskeline
+import Control.Monad.IO.Class (liftIO)
+
+process :: String -> IO ()
+process line = do
+    case parse programP "<stdin>" line of
+        Left err -> print err
+        Right prog -> print prog
+
+
+main :: IO ()  -- REPL
+main = runInputT defaultSettings loop
+    where
+        loop :: InputT IO ()
+        loop = do
+            minput <- getInputLine "womp> "
+            case minput of
+                Nothing     -> return ()
+                Just input  -> (liftIO $ process input) >> loop
