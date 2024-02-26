4eddeeb505c0d433b40ea43713091d78436fb69c
Markus Le Roux <markusleroux314@gmail.com>
2024-02-25 20:04:59 -0500

    Basic pretty printing

--- src/AST.hs
+++ src/AST.hs
@@ -1,5 +1,6 @@
 module AST where
 
+import Prettyprinter
 
 data Op
     = Add
@@ -8,10 +9,22 @@ data Op
     | Div
     deriving (Eq, Ord, Show)
 
+instance Pretty Op where
+    pretty Add  = pretty "+"
+    pretty Sub  = pretty "-"
+    pretty Mult = pretty "*"
+    pretty Div  = pretty "/"
+
+
 data Type
     = TInt
     | TFloat
     deriving (Eq, Ord, Show)
+
+instance Pretty Type where
+    pretty TInt    = pretty "int"
+    pretty TFloat  = pretty "float"
+
 
 type Name = String
 
@@ -19,6 +32,11 @@ data Variable
     = UntypedVar Name
     | TypedVar Type Name
     deriving (Eq, Ord, Show)
+
+instance Pretty Variable where
+    pretty (UntypedVar name)      = pretty name
+    pretty (TypedVar type_ name)  = pretty type_ <+> pretty name
+
 
 data Expr
     = IntLit Integer
@@ -29,8 +47,21 @@ data Expr
     | Assignment Expr Expr
     deriving (Eq, Ord, Show)
 
+instance Pretty Expr where
+    pretty (IntLit val)         = pretty val
+    pretty (FloatLit val)       = pretty val
+    pretty (Variable var)       = pretty var
+    pretty (BinOp op expl expr) = pretty expl <+> pretty op <+> pretty expr
+    pretty (Call name exps)     = pretty name <> align (tupled $ pretty <$> exps)
+    pretty (Assignment l r)     = pretty l <+> equals <+> pretty r
+
+
 data Block = Block [Expr]
     deriving (Eq, Ord, Show)
+
+instance Pretty Block where
+    pretty (Block exprs) = braces $ vsep $ (<> semi) . pretty <$> exprs
+
 
 data Function = Function
     { functionName :: Name
@@ -39,6 +70,15 @@ data Function = Function
     , functionBody :: Block
     }
     deriving (Eq, Ord, Show)
+
+instance Pretty Function where
+    pretty fn = pretty "fn"
+            <+> pretty (functionName fn)
+             <> align  (tupled $ pretty <$> functionArguments fn)
+            <+> pretty "->"
+            <+> pretty (functionReturnType fn)
+            <+> pretty (functionBody fn)
+
 
 data Program = Program
     { globals :: [Expr]
