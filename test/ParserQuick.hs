f3fb8c6545400bbcfcced08fa374abc041ed3ed8
Markus Le Roux <markusleroux314@gmail.com>
2024-03-01 15:04:19 -0500

    QuickCheck blockP, functionP and programP

--- test/ParserQuick.hs
+++ test/ParserQuick.hs
@@ -2,6 +2,7 @@ module ParserQuick where
 
 import AST
 import Parser
+import Lexer (identifier)
 
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
@@ -12,7 +13,9 @@ import Text.Parsec.String (Parser)
 import Prettyprinter
 import Prettyprinter.Render.String
 
-import Control.Monad (liftM2, liftM3)
+import Control.Monad (liftM2, liftM3, liftM4)
+import Data.Either (isRight)
+
 
 instance Arbitrary AST.UnOp where
     arbitrary = elements [ Neg, Pos ]
@@ -23,7 +26,10 @@ instance Arbitrary AST.BinOp where
 instance Arbitrary AST.Type where
     arbitrary = elements [ TInt, TFloat ]
 
-arbitraryIdentifier = listOf1 $ elements ['a'..'z']  -- TODO: more identifiers, don't generate reserved
+arbitraryIdentifier = (listOf1 $ elements ['_','a'..'z']) `QC.suchThat` (canParse identifier)
+    where
+        canParse :: Parser a -> String -> Bool
+        canParse p s = isRight $ parse p "" s
 
 instance Arbitrary AST.Variable where
     arbitrary = QC.oneof
@@ -51,6 +57,15 @@ instance Arbitrary AST.Expr where
                         k <- QC.choose (0, n)
                         QC.vectorOf k $ ast ( n `div` k :: Int )
 
+instance Arbitrary AST.Block where
+    arbitrary = Block <$> arbitrary
+
+instance Arbitrary AST.Function where
+    arbitrary = liftM4 Function arbitraryIdentifier arbitrary arbitrary arbitrary
+
+instance Arbitrary AST.Program where
+    arbitrary = liftM2 Program arbitrary arbitrary
+
 
 prop_prettyParserInverse :: (Show a, Pretty a, Eq a, Arbitrary a) => Parser a -> a -> Property
 prop_prettyParserInverse parser ast =
@@ -62,9 +77,16 @@ prop_prettyParserInverse parser ast =
             Right a -> ast === a
 
 
-parsingQuickTests = testGroup "Pretty followed by parse is identify"
-    [ QC.testProperty "typeP"     $ prop_prettyParserInverse typeP
-    , QC.testProperty "variableP" $ prop_prettyParserInverse variableP
-    , QC.testProperty "exprP"     $ prop_prettyParserInverse exprP
-    ]
+parsingQuickTests 
+    -- TODO: how to do this properly?
+    = adjustOption (const $ QC.QuickCheckMaxSize 5) 
+    $ adjustOption (const $ QC.QuickCheckTests 1000)
+    $ testGroup "Pretty followed by parse is identify"
+        [ QC.testProperty "typeP"     $ prop_prettyParserInverse typeP
+        , QC.testProperty "variableP" $ prop_prettyParserInverse variableP
+        , QC.testProperty "exprP"     $ prop_prettyParserInverse exprP
+        , QC.testProperty "blockP"    $ prop_prettyParserInverse blockP
+        , QC.testProperty "functionP" $ prop_prettyParserInverse functionP
+        , QC.testProperty "programP"  $ prop_prettyParserInverse programP
+        ]
 
