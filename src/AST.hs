ad998aa1a3b2d0a8d91244542643f1fc296b5741
Markus Le Roux <markusleroux314@gmail.com>
2024-03-02 12:33:42 -0500

    Move pretty printer into module

--- src/AST.hs
+++ src/AST.hs
@@ -1,16 +1,9 @@
 module AST where
 
-import Prettyprinter
-import Data.Bool
-
 data UnOp
     = Neg
     | Pos
     deriving (Eq, Ord, Show)
-
-instance Pretty UnOp where
-    pretty Neg = pretty "-"
-    pretty Pos = pretty "+"
 
 
 data BinOp
@@ -20,21 +13,11 @@ data BinOp
     | Div
     deriving (Eq, Ord, Show)
 
-instance Pretty BinOp where
-    pretty Add  = pretty "+"
-    pretty Sub  = pretty "-"
-    pretty Mult = pretty "*"
-    pretty Div  = pretty "/"
-
 
 data Type
     = TInt
     | TFloat
     deriving (Eq, Ord, Show)
-
-instance Pretty Type where
-    pretty TInt    = pretty "int"
-    pretty TFloat  = pretty "float"
 
 
 type Name = String
@@ -43,11 +26,6 @@ data Variable
     = UntypedVar Name
     | TypedVar Type Name
     deriving (Eq, Ord, Show)
-
-instance Pretty Variable where
-    pretty (UntypedVar name)      = pretty name
-    pretty (TypedVar type_ name)  = pretty type_ <+> pretty name
-
 
 data Expr
     = IntLit Integer
@@ -59,31 +37,8 @@ data Expr
     | Assignment Variable Expr
     deriving (Eq, Ord, Show)
 
-instance Pretty Expr where
-    pretty (IntLit val)         = pretty val
-    pretty (FloatLit val)       = pretty val
-    pretty (Variable var)       = pretty var
-    pretty (UnOp op exp)        = pretty op <> prettyGrouped exp
-    pretty (BinOp op expl expr) = prettyGrouped expl <+> pretty op <+> prettyGrouped expr
-    pretty (Call name exps)     = pretty name <> align (tupled $ pretty <$> exps)
-    pretty (Assignment l r)     = pretty l <+> equals <+> pretty r
-        
-prettyGrouped lit@(IntLit _)   = pretty lit
-prettyGrouped lit@(FloatLit _) = pretty lit
-prettyGrouped var@(Variable _) = pretty var
-prettyGrouped call@(Call _ _)  = pretty call
-prettyGrouped p                = pretty "(" <> pretty p <> pretty ")"
-
 newtype Block = Block [Expr]
     deriving (Eq, Ord, Show)
-
-instance Pretty Block where
-    pretty (Block exprs) = braces $ multilineMb (null exprs) $ vsep $ prettyStatement <$> exprs
-        where 
-            multilineMb = bool (enclose line line) id
-
-prettyStatement :: Pretty a => a -> Doc ann
-prettyStatement = (<> semi) . pretty
 
 
 data Function = Function
@@ -94,21 +49,8 @@ data Function = Function
     }
     deriving (Eq, Ord, Show)
 
-instance Pretty Function where
-    pretty fn = pretty "fn"
-            <+> pretty (functionName fn)
-             <> align  (tupled $ pretty <$> functionArguments fn)
-            <+> pretty "->"
-            <+> pretty (functionReturnType fn)
-            <+> pretty (functionBody fn)
-
-
 data Program = Program
     { globals :: [Expr]
     , functions :: [Function]
     }
     deriving (Eq, Ord, Show)
-
-instance Pretty Program where
-    pretty (Program exprs functions) = vsep $ (prettyStatement <$> exprs) <> (prettyStatement <$> functions)
-
