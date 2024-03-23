1ebcf04135f1f988aa59ec8e6a29f6ba4143e5f9
Markus Le Roux <markusleroux314@gmail.com>
2024-03-23 15:58:21 -0400

    IR printing in REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,54 @@
 module Main where
 
-import Parser (programP)
+import Parser (programP, exprP)
+import qualified AST
 
 import Control.Monad.IO.Class (liftIO)
 import System.Console.Haskeline (InputT, defaultSettings, getInputLine, runInputT)
 import Text.Parsec (parse)
+import LLVM.Pretty (ppllvm)
+import qualified Data.Text.Lazy.IO as TIO
 
-process :: String -> IO ()
-process line = do
-    case parse programP "<stdin>" line of
+import LLVM
+import Options.Applicative
+
+data Options = Options
+  { ir :: Bool
+  }
+
+commandLine :: Parser Options
+commandLine = Options
+  <$> switch (long "ir" <> short 'i' <> help "Print the IR to stdout")
+
+data Printers = Printers
+  { astPrinter :: Maybe (AST.Expr AST.Name -> IO ())
+  , irPrinter :: Maybe (AST.Expr AST.Name -> IO ())
+  }
+
+fromCommandLine :: Options -> Printers
+fromCommandLine Options{..} = Printers
+  { astPrinter = Just print
+  , irPrinter = if ir then Just (TIO.putStrLn . ppllvm . toLLVM) else Nothing
+  }
+
+runPrinters :: Printers -> AST.Expr AST.Name -> IO ()
+runPrinters Printers{..} e = do
+  sequence $ astPrinter <*> pure e
+  sequence $ irPrinter <*> pure e
+  pure ()
+
+
+doProcessing :: Printers -> String -> IO ()
+doProcessing p line = do
+    case parse exprP "<stdin>" line of
         Left err -> print err
-        Right prog -> print prog
+        Right prog -> runPrinters p prog
 
 main :: IO () -- REPL
-main = runInputT defaultSettings loop
-  where
-    loop :: InputT IO ()
-    loop = do
-        minput <- getInputLine "womp> "
-        case minput of
-            Nothing -> return ()
-            Just input -> liftIO (process input) >> loop
+main = do
+  options <- execParser $ info commandLine fullDesc
+  runInputT defaultSettings $ loop (fromCommandLine options)
+    where
+      loop p = getInputLine "womp> " >>= \case
+        Nothing -> return ()
+        Just input -> liftIO (doProcessing p input) >> loop p
