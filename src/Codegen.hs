2be43ba266c1e1b39ce99234697b1518cf754c9e
Markus Le Roux <markusleroux314@gmail.com>
2024-03-29 20:28:44 -0400

    Minor clean up in codegen

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -16,17 +16,32 @@ module Codegen
 
 import ASTToLLVM (exprToLLVM, toLLVM, CodegenError)
 
-import Foreign.Ptr
+import Foreign.Ptr (FunPtr, castFunPtr)
 
 import qualified LLVM.AST
 
 import LLVM.Passes
-import LLVM.Analysis
-import LLVM.Module
-import LLVM.Context
-import LLVM.ExecutionEngine as EE
+import LLVM.Analysis (verify)
+import LLVM.Module (moduleAST, moduleLLVMAssembly, withModuleFromAST)
+import LLVM.Context (withContext, Context)
+import LLVM.ExecutionEngine as EE (withMCJIT, ExecutionEngine(getFunction, withModuleInEngine), MCJIT)
 
 import qualified Data.ByteString as B
+
+
+jit :: Context -> (EE.MCJIT -> IO a) -> IO a
+jit c = EE.withMCJIT c optlevel model ptrelim fastins
+  where
+    optlevel = Just 0
+    model    = Nothing
+    ptrelim  = Nothing
+    fastins  = Nothing
+
+foreign import ccall "dynamic" 
+  haskFun :: FunPtr (IO Int) -> IO Int
+
+run :: FunPtr a -> IO Int
+run fn = haskFun (castFunPtr fn :: FunPtr (IO Int))
 
 
 optimize :: LLVM.AST.Module -> IO LLVM.AST.Module
@@ -56,18 +71,3 @@ optimize astMod = do
         pure optmod
   where
     passSpec = PassSetSpec [CuratedPassSet 3] Nothing
-
-jit :: Context -> (EE.MCJIT -> IO a) -> IO a
-jit c = EE.withMCJIT c optlevel model ptrelim fastins
-  where
-    optlevel = Just 0
-    model    = Nothing
-    ptrelim  = Nothing
-    fastins  = Nothing
-
-foreign import ccall "dynamic" 
-  haskFun :: FunPtr (IO Int) -> IO Int
-
-run :: FunPtr a -> IO Int
-run fn = haskFun (castFunPtr fn :: FunPtr (IO Int))
-
